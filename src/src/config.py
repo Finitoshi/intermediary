c44fdaebc3885eaf345c8d34c05b748e5bfef933
Finitoshi <93954338+Finitoshi@users.noreply.github.com>
2024-12-03 02:50:16 -0500

    Create config.py

---
+++ src/src/config.py
@@ -0,0 +1,23 @@
+
+### `src/config.py`:
+
+```python
+# src/config.py
+
+import os
+
+# Load all the cool secrets from the environment - because we're not about to spill the beans here
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+GROK_API_KEY = os.getenv("GROK_API_KEY")
+GROK_API_URL = os.getenv("GROK_API_URL")
+JWK_PATH = os.getenv("JWK_PATH")
+HUGGINGFACE_API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
+HUGGINGFACE_SPACE_URL = os.getenv("HUGGINGFACE_SPACE_URL")
+RENDER_INTERMEDIARY_URL = os.getenv("RENDER_INTERMEDIARY_URL")
+RENDER_TG_BOT_WEBHOOK_URL = os.getenv("RENDER_TG_BOT_WEBHOOK_URL")
+MONGO_URI = os.getenv("MONGO_URI")
+CHIBI_TG_KEY_GROK = os.getenv("CHIBI_TG_KEY_GROK")
+PORT = int(os.getenv("PORT", 8000))  # Default to 8000 if you forgot to set it, because YOLO
+
+# Let's keep this URL in case we want to make this service chat with itself or something
+INTERMEDIARY_URL = RENDER_INTERMEDIARY_URL  # Use the Render URL for the intermediary
